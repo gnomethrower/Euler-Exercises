23fa58c2f8e29a7199352cc9c65e2d702c24f749
gnomethrower <robrobusa@yahoo.de>
2017-06-23 21:43:31 +0200

    Update euler2.cpp

--- euler2.cpp
+++ euler2.cpp
@@ -1,8 +1,23 @@
 #include <iostream>
 using namespace std;
 
+int main() {
 
-int main()
-{
+int f=0;
+int x=1;
+int y=2;
+int z=0;
+	
+for( z = x;f<=20; f=x+y ){
+z=x;
+x=y;
+y=y+z;
+	
+cout << x << y << f;	
+	
+	
+	
+}
+
 	return 0;
 }
