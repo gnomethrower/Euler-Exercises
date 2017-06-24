eb3fe819fbd26870d654544f049b4634f14576e0
gnomethrower <robrobusa@yahoo.de>
2017-06-24 13:37:54 +0200

    Update euler5.cpp

--- euler5.cpp
+++ euler5.cpp
@@ -3,8 +3,28 @@ using namespace std;
 
 int main() {
 
+int i = 0;//interval
+int m = 1;
+
+if#include <iostream>
+using namespace std;
+
+int main() {
+
+int i = 0;
+int m = 1;
+	
+while(m<20){
+	m++;
+	while(i%m==0){
+		i++;
+	}
+}
+cout << i << endl;
+	
+	
+	
+	
 
 	return 0;
-
-
 }
